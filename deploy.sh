6e0eb5a30b396488a3ff5f598d83d749a3221800
srfoster <stephen@thoughtstem.com>
2019-11-12 13:16:06 -0800

    tweaking deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 rm -rf out
+git worktree prune
 git worktree add out gh-pages
 racket main.rkt
 cd out/
 git add --all
 git commit -m "deploy"
-git push origin gh-pages
+git push -f origin gh-pages
