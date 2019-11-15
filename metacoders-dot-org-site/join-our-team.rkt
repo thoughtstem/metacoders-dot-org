7a63a7704685a014ceb326af1e27ef5644954def
Lindsey Handley <lindseyhandley@gmail.com>
2019-11-15 15:13:50 -0800

    finished first pass on join our team page

--- metacoders-dot-org-site/join-our-team.rkt
+++ metacoders-dot-org-site/join-our-team.rkt
@@ -87,5 +87,24 @@
                            margin-bottom: 0
                            background: "white")
     (container
-      (h1 "Our Core Value")
-      (p "We be good people."))))
+      (h1 "Our Core Values")
+      (br)
+      (p "We're all in this together! Our core values help us accomplish our mission of creating a world where everyone knows how to code.")
+      (br)
+      (card-deck
+        (card class: "text-white bg-primary"
+          (card-header class: "font-weight-bold" "Compassion")
+          (card-body
+            (card-text "A common phrase we use: \"we be good people\". We always exercise compassion when working with our employees, volunteers, parent customers, and students.")))
+        (card class: "text-white bg-primary"
+          (card-header class: "font-weight-bold" "Resourcefulness")
+          (card-body
+            (card-text "It doesn't take the most expensive equipment or the flashiest software to teach coding. Many of us learned at home on our own! Resourcefulness is one way we offer better classes at lower costs.")))
+        (card class: "text-white bg-primary"
+          (card-header class: "font-weight-bold" "Adaptability")
+          (card-body
+            (card-text "We use coding in everything. Our entire team, from coaches to administrators, are always looking for ways to be even more adaptable using the power of code.")))
+        (card class: "text-white bg-primary"
+          (card-header class: "font-weight-bold" "Respect")
+          (card-body
+            (card-text "We respect people of all backgrounds. Respect between teachers and students is paramount in the classroom. The same goes for respect between all levels of staff at MetaCoders.")))))))
