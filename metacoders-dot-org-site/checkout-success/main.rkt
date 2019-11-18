9f50512dad845faae2ed1330b8e633b44cc2ad06
sonnynajar <sonny.najar@gmail.com>
2019-11-18 10:26:32 -0800

    Update main.rkt

--- metacoders-dot-org-site/checkout-success/main.rkt
+++ metacoders-dot-org-site/checkout-success/main.rkt
@@ -6,14 +6,17 @@
 (define (checkout-success)
   (page checkout-success-top-path
         (normal-content
-          (h1 "Payment Success!")
+          (div style: (properties
+                       text-align: "center"
+                       height: 500
+                       padding-top: 50)(h1 "Payment Success!")
           (p "You will receive an email with your receipt shortly.")
           (br)
           (h4 "Now we need the student's details!")
-          (p "Click on the button below to open the form.")
+          (p "Click here to fill out the registration information for your student:")
           (a href:"https://docs.google.com/forms/d/e/1FAIpQLSeo6vHe7gyNLl-BLNfOWIGmHyj3tN7Y7WNhsfH49DokINyt5Q/viewform"
              class:"btn btn-info"
              role:"button"
              "Finish Enrollment")
           (br)
-          )))
+          ))))
