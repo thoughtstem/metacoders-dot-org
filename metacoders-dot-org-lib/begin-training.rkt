46b8b8403a19d6910c2e5b842bc09dc1e1bbf78d
Lindsey Handley <lindseyhandley@gmail.com>
2019-12-09 14:13:56 -0800

    employment page cleanup and new city page images

--- metacoders-dot-org-lib/begin-training.rkt
+++ metacoders-dot-org-lib/begin-training.rkt
@@ -4,14 +4,19 @@
 
 (require website/bootstrap)
 
-(define (begin-training description target-page [classes ""])
+(define (begin-training #:training-exists? [training-exists? #t] 
+                        description target-page [classes ""])
   (card class: classes
     (card-body
       (card-title "You Can Start Training Now!")
       (card-text
         description
-        (link-to target-page
-                 (button class: "btn btn-primary" 
-                         "Begin Training!"))))))
+        (if (false? training-exists?)
+          (h6 style: (properties color: "red"
+                                 text-align: "center") 
+              "This Training is Coming Soon!")
+          (link-to target-page
+            (button class: "btn btn-primary" 
+                   "Begin Training!")))))))
 
 
