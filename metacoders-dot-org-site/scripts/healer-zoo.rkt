d8814e2760fe210df947b09d1f35a2bf3fb08da0
sonnynajar <sonny.najar@gmail.com>
2019-12-09 13:45:56 -0800

    zoo script

---
+++ metacoders-dot-org-site/scripts/healer-zoo.rkt
@@ -0,0 +1,36 @@
+#lang at-exp racket
+
+ (require mc-scripts)
+
+ ;--- Define pkg(s) test(s)
+ (define healer-zoo-foods-test
+   @~a{#lang healer-zoo-foods
+       (start hippo (fish grapes apple))})
+
+ (define healer-zoo-friends-test
+   @~a{#lang healer-zoo-friends
+       (start
+         (rand orange)
+         ((fish yellow) (tomato green) (apple 2))
+         ((monkey 2) (rand purple)))})
+
+ (define healer-zoo-enemies-test
+   @~a{#lang healer-zoo-enemies 
+       (start
+         rand
+         ((apple yellow 5))
+         ((apple blue 5))
+         ((apple green 5)))})
+
+ ;--- Check pkg(s)
+ (system "raco pkg remove --force reprovide-lang") ;<--- remove old pkg
+
+ (check-pkg (list "healer-zoo-foods"
+                  "healer-zoo-friends"
+                  "healer-zoo-enemies")
+            (list "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-zoo-foods"
+                  "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-zoo-friends"
+                  "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-zoo-enemies")
+            (list healer-zoo-foods-test
+                  healer-zoo-friends-test
+                  healer-zoo-enemies-test))
