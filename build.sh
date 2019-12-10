ec12befd18b66e732e7d98c3dfa74e1eb4981206
sonnynajar <sonny.najar@gmail.com>
2019-12-10 14:25:45 -0800

    travis tester

---
+++ build.sh
@@ -0,0 +1,15 @@
+#Exit if commands fail.  Fail fast!
+set -e
+
+echo "**************************"
+echo "INSTALLING metacoders-dot-org-lib"
+echo "**************************"
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast metacoders-dot-org-lib
+
+echo "**************************"
+echo "INSTALLING metacoders-dot-org-lib"
+echo "**************************"
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-site#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast metacoders-dot-org-site
+
