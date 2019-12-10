7280cff0b2de29739ddf66693cb508e3a5e0d4ab
sonnynajar <sonny.najar@gmail.com>
2019-12-10 15:11:31 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,24 @@ echo "**************************"
 echo "INSTALLING metacoders-dot-org-lib"
 echo "**************************"
 raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib#$TRAVIS_BRANCH
+
+echo "**************************"
+echo "RUNNING raco setup metacoders-dot-org-lib"
+echo "**************************"
 raco setup --no-docs --fail-fast metacoders-dot-org-lib
 
 echo "**************************"
 echo "INSTALLING metacoders-dot-org-site"
 echo "**************************"
 raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-site#$TRAVIS_BRANCH
+
+echo "**************************"
+echo "RUNNING raco setup mode-lambda"
+echo "**************************"
+raco setup mode-lambda
+
+echo "**************************"
+echo "RUNNING raco setup metacoders-dot-org-site"
+echo "**************************"
 raco setup --no-docs --fail-fast metacoders-dot-org-site
 
