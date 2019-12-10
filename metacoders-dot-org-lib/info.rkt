dd39153010ad8f8bf3ad445a17fe519ba258ebac
sonnynajar <sonny.najar@gmail.com>
2019-12-10 14:38:53 -0800

    Update info.rkt

--- metacoders-dot-org-lib/info.rkt
+++ metacoders-dot-org-lib/info.rkt
@@ -2,6 +2,7 @@
 (define deps '("base"
                "reprovide-lang"
                "gregor"
+               "jack-posn"
                "https://github.com/thoughtstem/website.git"
                "https://github.com/thoughtstem/impress.git"
                "https://github.com/thoughtstem/metapolis-stories.git"))
