841ac8ef3b5c0f362eb15e9fcc045133d694b727
srfoster <stephen@thoughtstem.com>
2019-12-11 12:42:32 -0800

    dep

--- metacoders-dot-org-lib/info.rkt
+++ metacoders-dot-org-lib/info.rkt
@@ -6,4 +6,6 @@
                "jack-posn"
                "https://github.com/thoughtstem/website.git"
                "https://github.com/thoughtstem/impress.git"
-               "https://github.com/thoughtstem/metapolis-stories.git"))
+               "https://github.com/thoughtstem/metapolis-stories.git"
+               "https://github.com/thoughtstem/mc-languages.git"
+               ))
