e18f442aec6b9343631c91def494f153b3954b97
sonnynajar <sonny.najar@gmail.com>
2020-02-06 07:19:01 -0800

    Update initial-script.rkt

--- metacoders-dot-org-site/scripts/initial-script.rkt
+++ metacoders-dot-org-site/scripts/initial-script.rkt
@@ -11,6 +11,7 @@
  sudo apt-get install racket -y
  raco pkg install --deps search-auto --update-deps https://github.com/thoughtstem/mc-scripts.git
  racket -l mc-scripts/change-bg
+ racket -l mc-scripts/racket-prefs
  })
 
 (define config.sh
