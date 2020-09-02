e9cd46efb45d2a85f73162f55577e81599a447b0
sonnynajar <sonny.najar@gmail.com>
2020-09-02 11:37:56 -0700

    added cpx img

--- metacoders-dot-org-lib/imgs.rkt
+++ metacoders-dot-org-lib/imgs.rkt
@@ -219,6 +219,8 @@
   (list "videos" "coding-maze-pokemon-game.mp4"))
 (define/provide/image python-img-path python-img-webp-path
   (list "img" "online" "python-game-with-code.jpg"))
+(define/provide/image cpx-img-path cpx-img-webp-path
+  (list "img" "online" "virtual-engineering-cpx.jpg"))
 ;; QCC End
 
 (define/provide/image sched-value-img-path sched-value-img-webp-path
